cadfd0583b2e5fd0b43dd5c349af1a2a99f6f60c
janicejanekim <janicejkimani@gmail.com>
2022-10-18 12:48:35 +0300

    wpl

--- teachers survey.R
+++ teachers survey.R
@@ -142,6 +142,7 @@ survey<-skills_assessment%>%
   summarise(cnt = n())%>%
   mutate(freq = round(cnt / sum(21),2)) %>% 
   arrange(desc(freq))
+
 ggplot(survey,aes(x = survey,y = freq,fill = survey,label=scales::percent(freq))) +
   geom_bar(position="dodge",stat="identity",width = 0.3) +
   scale_fill_manual(values = c("chartreuse2","mediumseagreen"))+
@@ -172,7 +173,17 @@ Gender_devices<-skills_assessment%>%
   tabyl(`own devices`,Gender)%>%
   adorn_title()
 
+#####
 
+
+# 
+# wpl<-skills_assessment%>%
+#   select(Gender,school,survey,`own devices`,`oftenly use Devices`,online)%>%
+#   pivot_longer(!c(Gender,school), names_to = "reason_cycling", values_to = "Responses")%>%
+#   group_by(`own devices`)%>%
+#   summarise(cnt = n())%>%
+#   mutate(freq = round(cnt / sum(21),2)) %>% 
+#   arrange(desc(freq))
 
 ####
   Devices<-skills_assessment%>%
@@ -204,12 +215,183 @@ Gender_devices<-skills_assessment%>%
           legend.text = element_text(color = "black", size = 7,face = "bold"),
           plot.title = element_text(face = "bold",hjust = 0.5,size = 10))
 ggsave("devices.png")
+#####
 
 ####
 Devices_use<-skills_assessment%>%
-  select(Gender,school,survey,`own devices`,`oftenly use Devices`)%>%
-  group_by(Gender,`own devices`,`oftenly use Devices`)%>%
+  select(Gender,school,survey,`own devices`,`oftenly use Devices`,online,`Reason for buying device`)%>%
+  group_by(`own devices`,online)%>%
   summarise(cnt = n())%>%
   mutate(freq = round(cnt / sum(21),2)) %>% 
   arrange(desc(freq))
+
+ggplot(Devices_use,aes(x = `own devices`,y = freq,fill = `own devices`,
+                       label=scales::percent(freq)))+
+  geom_bar(position="dodge",stat="identity",width = 0.4)+
+  scale_fill_manual(values = c("mediumseagreen","chartreuse2","chocolate2",
+                               "darkturquoise","darkgoldenrod2"))+
+  scale_y_continuous(labels = scales::percent,
+                     breaks = scales::pretty_breaks(n = 7))+
+  geom_text(nudge_y= .01,
+            color="black",
+            size = 4,
+            fontface="bold")+
+  facet_wrap(~online)+
+  labs(x = "Devices",
+       title = "percentages of devices owned grouped by usage ")+
+  theme(legend.position = "right",
+        strip.text.x = element_text(size = 10),
+        axis.text.x = element_blank(),
+        axis.text.y = element_text(color="black", size = 10, angle = 0,face = "bold"),
+        axis.title.x = element_text(colour="black", size = 10,face = "bold"),
+        axis.title.y = element_text(colour="black", size = 15,face = "bold",hjust = 0.5),
+        legend.title = element_text(color = "black", size = 5,face = "bold"),
+        legend.text = element_text(color = "black", size = 7,face = "bold"),
+        plot.title = element_text(face = "bold",hjust = 0.5,size = 10))
+####
+reason_buying_device<-skills_assessment%>%
+  select(Gender,`own devices`,`oftenly use Devices`,online,`Reason for buying device`,`use for internet data`)%>%
+  group_by(`use for internet data`)%>%
+  summarise(cnt = n())%>%
+  mutate(freq = round(cnt / sum(21),2)) %>% 
+  arrange(desc(freq))
+
+
+ggplot(reason_buying_device,aes(x =`use for internet data`,y = freq,fill = `use for internet data`,
+                       label=scales::percent(freq)))+
+  geom_bar(position="dodge",stat="identity",width = 0.4)+
+  scale_fill_manual(values = c("mediumseagreen","chartreuse2","chocolate2",
+                               "darkturquoise","darkgoldenrod2"))+
+  scale_y_continuous(labels = scales::percent,
+                     breaks = scales::pretty_breaks(n = 7))+
+  geom_text(nudge_y= .01,
+            color="black",
+            size = 4,
+            fontface="bold")+
+  labs(x = "Devices",
+       title = "Usage for internet")+
+  theme(legend.position = "none",
+        strip.text.x = element_text(size = 10),
+        panel.spacing = unit(2, "lines"),
+        axis.text.x = element_text(color = "black", size = 7,angle = 0,face = "bold"),
+        axis.text.y = element_text(color="black", size = 10, angle = 0,face = "bold"),
+        axis.title.x = element_text(colour="black", size = 10,face = "bold"),
+        axis.title.y = element_text(colour="black", size = 15,face = "bold",hjust = 0.5),
+        legend.title = element_text(color = "black", size = 5,face = "bold"),
+        legend.text = element_text(color = "black", size = 7,face = "bold"),
+        plot.title = element_text(face = "bold",hjust = 0.5,size = 10))
+ggsave("usage.png")
+#####
+library(RColorBrewer)
+data<-skills_assessment%>%
+  select(Gender,`own devices`,`oftenly use Devices`,online,`Reason for buying device`,`use for internet data`)%>%
+  group_by(`use for internet data`)%>%
+  summarise(cnt = n())%>%
+  mutate(freq = round(cnt / sum(21),2)) %>% 
+  arrange(desc(freq))
+ggplot(data,aes(x =`use for internet data`,y = freq,fill = `use for internet data`,
+                                label=scales::percent(freq)))+
+  geom_bar(position="dodge",stat="identity",width = 0.4)+
+  scale_fill_manual(values = c("mediumseagreen","chartreuse2","chocolate2",
+                               "darkturquoise","darkgoldenrod2"))+
+scale_y_continuous(labels = scales::percent,
+                     breaks = scales::pretty_breaks(n = 7))+
+  geom_text(nudge_y= .01,
+            color="black",
+            size = 4,
+            fontface="bold")+
+  labs(x = "Devices",
+       title = "Usage for internet")+
+  theme(legend.position = "none",
+        strip.text.x = element_text(size = 10),
+        panel.spacing = unit(2, "lines"),
+        axis.text.x = element_text(color = "black", size = 7,angle = 0,face = "bold"),
+        axis.text.y = element_text(color="black", size = 10, angle = 0,face = "bold"),
+        axis.title.x = element_text(colour="black", size = 10,face = "bold"),
+        axis.title.y = element_text(colour="black", size = 15,face = "bold",hjust = 0.5),
+        legend.title = element_text(color = "black", size = 5,face = "bold"),
+        legend.text = element_text(color = "black", size = 7,face = "bold"),
+        plot.title = element_text(face = "bold",hjust = 0.5,size = 10))
+ggsave("data.png")
+#####
+
+# 
+# Barriers<-skills_assessment%>%
+#   select(Barriers)%>%
+#   mutate(Barriers,paying_for_internet=case_when(Barriers=="Paying for Internet is too much money for me"~1,Barriers=="Not enough power (charge)\r\nPaying for Internet is too much money for me"~1))
+#   summarise(cnt = n())%>%
+#   mutate(freq = round(cnt / sum(21),2)) %>% 
+#   arrange(desc(freq))%>%
+#   ungroup()
+
+####
+digital<-skills_assessment%>%
+  select(Gender,Devices,`Devices barriers`)%>%
+  group_by(Gender,Devices)%>%
+  summarise(cnt = n())%>%
+  mutate(freq = round(cnt / sum(21),2)) %>% 
+  arrange(desc(freq))
+
+
+ggplot(digital,aes(x=Devices,y = freq,fill = Devices,
+                label=scales::percent(freq)))+
+  geom_bar(position="dodge",stat="identity",width = 0.4)+
+  scale_fill_manual(values = c("mediumseagreen","chartreuse2",
+                               "darkturquoise"))+
+  scale_y_continuous(labels = scales::percent,
+                     breaks = scales::pretty_breaks(n = 7))+
+  facet_wrap(~Gender)+
+  geom_text(nudge_y= .01,
+            color="black",
+            size = 4,
+            fontface="bold")+
+  labs(x = "Devices",
+       title = "percentage of teachers on their level on using devices by gender")+
+  theme(legend.position = "none",
+        strip.text.x = element_text(size = 10),
+        panel.spacing = unit(2, "lines"),
+        axis.text.x = element_text(color = "black", size = 7,angle = 0,face = "bold"),
+        axis.text.y = element_text(color="black", size = 10, angle = 0,face = "bold"),
+        axis.title.x = element_text(colour="black", size = 10,face = "bold"),
+        axis.title.y = element_text(colour="black", size = 15,face = "bold",hjust = 0.5),
+        legend.title = element_text(color = "black", size = 5,face = "bold"),
+        legend.text = element_text(color = "black", size = 7,face = "bold"),
+        plot.title = element_text(face = "bold",hjust = 0.5,size = 10))
+ggsave("digital.png")  
+####  
+Work<-skills_assessment%>%
+  select(Gender,Devices,`work productivity`,APPs,Entertainment,communities,Education)%>%
+  pivot_longer(!c(Gender), names_to = "categories",values_to = "levels")%>%
+  group_by(categories,levels)%>%
+  summarise(cnt = n())%>%
+  mutate(freq = round(cnt / sum(21),2)) %>% 
+  arrange(desc(freq))
+
+  
+
+ggplot(Work,aes(x=levels,y = freq,fill = levels,
+                   label=scales::percent(freq)))+
+  geom_bar(stat="identity",width = 0.5)+
+  display.brewer.pal(n = 6, name = 'PuBu')+
+  scale_y_continuous(labels = scales::percent,
+                     breaks = scales::pretty_breaks(n = 5))+
+  facet_wrap(~categories)+
+  geom_text(nudge_y= .01,
+            color="black",
+            size = 2.5,
+            fontface="bold")+
+  labs(x = "levels",
+       title = "percentage of teachers level as i am digital in different categories")+
+  theme(legend.position = "bottom",
+        strip.text.x = element_text(size = 9),
+        panel.spacing = unit(2, "lines"),
+        axis.text.x = element_blank(),
+        axis.text.y = element_text(color="black", size = 6, angle = 0,face = "bold"),
+        axis.title.x = element_text(colour="black", size = 10,face = "bold"),
+        axis.title.y = element_text(colour="black", size = 10,face = "bold",hjust = 0.5),
+        legend.title = element_text(color = "black", size = 10,face = "bold"),
+        legend.text = element_text(color = "black", size = 7,face = "bold"),
+        plot.title = element_text(face = "bold",hjust = 0.5,size = 10))
+ggsave("digital.png")
+
 
